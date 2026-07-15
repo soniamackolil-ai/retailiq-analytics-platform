e1699c1737b7f218b8a40fa54bbf0c5ebbb8ea53
Sonia Mackolil <sonia.mackolil@gmail.com>
2026-07-15 15:31:26 -0400

    All dashboards complete

--- dbt/retailiq_dbt/models/marts/dim_customers.sql
+++ dbt/retailiq_dbt/models/marts/dim_customers.sql
@@ -23,7 +23,31 @@ SELECT
     COALESCE(cs.purchase_frequency, 0)           AS purchase_frequency,
     COALESCE(cs.avg_order_value, 0)              AS avg_order_value,
     COALESCE(cs.preferred_category, 'Unknown')   AS preferred_category,
-    COALESCE(cs.preferred_channel, 'Unknown')    AS preferred_channel
+    COALESCE(cs.preferred_channel, 'Unknown')    AS preferred_channel,
+    CASE
+    WHEN clv_predicted <= 6413  THEN 'Bronze'
+    WHEN clv_predicted <= 12803 THEN 'Silver'
+    WHEN clv_predicted <= 18959 THEN 'Gold'
+    WHEN clv_predicted > 18959  THEN 'Platinum'
+    ELSE 'Unknown'
+END AS clv_tier,
+-- Full province name for Power BI map
+CASE province
+    WHEN 'ON' THEN 'Ontario'
+    WHEN 'BC' THEN 'British Columbia'
+    WHEN 'AB' THEN 'Alberta'
+    WHEN 'QC' THEN 'Quebec'
+    WHEN 'MB' THEN 'Manitoba'
+    WHEN 'SK' THEN 'Saskatchewan'
+    WHEN 'NS' THEN 'Nova Scotia'
+    WHEN 'NB' THEN 'New Brunswick'
+    WHEN 'NL' THEN 'Newfoundland and Labrador'
+    WHEN 'PE' THEN 'Prince Edward Island'
+    WHEN 'YT' THEN 'Yukon'
+    WHEN 'NT' THEN 'Northwest Territories'
+    WHEN 'NU' THEN 'Nunavut'
+    ELSE province
+END AS province_name
 FROM RETAILIQ_DB.STAGING.CUSTOMER_MASTER_CLEAN cm
 LEFT JOIN RETAILIQ_DB.STAGING.CUSTOMER_SEGMENTS_CLEAN cs
     ON cm.customer_id = cs.customer_id
--- dbt/retailiq_dbt/models/marts/dim_stores.sql
+++ dbt/retailiq_dbt/models/marts/dim_stores.sql
@@ -12,5 +12,21 @@ SELECT
     store_manager,
     phone,
     is_active,
-    region
+    region,
+    CASE province
+    WHEN 'ON' THEN 'Ontario'
+    WHEN 'BC' THEN 'British Columbia'
+    WHEN 'AB' THEN 'Alberta'
+    WHEN 'QC' THEN 'Quebec'
+    WHEN 'MB' THEN 'Manitoba'
+    WHEN 'SK' THEN 'Saskatchewan'
+    WHEN 'NS' THEN 'Nova Scotia'
+    WHEN 'NB' THEN 'New Brunswick'
+    WHEN 'NL' THEN 'Newfoundland and Labrador'
+    WHEN 'PE' THEN 'Prince Edward Island'
+    WHEN 'YT' THEN 'Yukon'
+    WHEN 'NT' THEN 'Northwest Territories'
+    WHEN 'NU' THEN 'Nunavut'
+    ELSE province
+END AS province_name
 FROM RETAILIQ_DB.STAGING.STORES_CLEAN
